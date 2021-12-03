f0f62bbbd4dae1986bc5141fe043e2b17873f8b8
lovekush rajput <lovekushrazput143@gmail.com>
2021-12-03 18:12:15 +0530

    block-BJaaet

---
+++ block-BJaaet/code/script.js
@@ -0,0 +1,30 @@
+let input = document.querySelector("input")
+let form = document.querySelector("form")
+let ul = document.querySelector("ul")
+
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+        let text = event.target.elements.text.value
+        event.target.elements.text.value = ""
+        let spanElm = document.createElement("span")
+        let li = document.createElement("li")
+        spanElm.style.marginRight = "3rem"
+        spanElm.innerText=text
+        let check = document.createElement("input");
+        check.setAttribute("type", "checkbox")
+        let close = document.createElement("span")
+        close.innerText = "❌"
+        li.append(check,spanElm,close)
+        ul.append(li)
+
+
+        close.addEventListener("click",handleClick)
+        function handleClick (){
+            if(check.checked == true){
+                li.remove();
+            }
+        }
+});
+
+
